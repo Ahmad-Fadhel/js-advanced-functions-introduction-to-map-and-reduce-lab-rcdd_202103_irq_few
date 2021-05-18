2ce74073bb844d7665417421d7fe8db480347152
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-05-18 22:57:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,54 +1,52 @@
 // Your code here
-function mapToNegativize(array) {
-    let newArray = []
-    for (let i = 0; i < array.length; i++ ) {
-      newArray.push(-1 * array[i])
-    }
-    return newArray
-  }
+// function mapToNegativize(sourceArray){
+//   let newArray = []
+//   sourceArray.forEach(element => newArray.push(-1 * element))
+//   return newArray
+// };
 
-  function mapToNoChange(array) {
-    let newArray = []
-    for (let i = 0; i < array.length; i++ ) {
-      newArray.push(array[i])
-    }
-    return newArray
-  }
+const mapToNegativize = sourceArray.map(array => {
+  return array* -1
+})
 
-  function mapToDouble(array) {
-    let newArray = []
-    for (let i = 0; i < array.length; i++ ) {
-      newArray.push(2 * array[i])
-    }
-    return newArray
-  }
+function mapToNoChange(sourceArray){
+  return sourceArray
+};
 
-  function mapToSquare(array) {
-    let newArray = []
-    for (let i = 0; i < array.length; i++ ) {
-      newArray.push(array[i] ** 2)
-    }
-    return newArray
-  }
+function mapToDouble(sourceArray){
+  let newArray = []
+  sourceArray.forEach(element => newArray.push(element * 2))
+  return newArray
+};
 
-  function reduceToTotal(array, startingPoint=0) {
-    let total = startingPoint
-    for (let i = 0; i < array.length; i++ ) {
-      total = total + array[i]
-    }
-    return total
-  }
+function mapToSquare(sourceArray){
+  let newArray = []
+  sourceArray.forEach(element => newArray.push(element * element))
+  return newArray
+};
 
-  function reduceToAllTrue(array) {
-    for (let i = 0; i < array.length; i++ ) {
-      if (!array[i]) return false
-    }
-    return true
-  }
+function reduceToTotal(sourceArray, startingPoint = 0){
+  let total = startingPoint
+  sourceArray.forEach(element => total += element)
+  return total
+};
 
-  function reduceToAnyTrue(array) {
-    for (let i = 0; i < array.length; i++ ) {
-      if (array[i]) return true
+function reduceToAllTrue(sourceArray){
+  let returnValue = true
+  sourceArray.forEach(function(value){
+    if (value == false){
+    returnValue = false
     }
-    return false
-  } 
+  })
+  return returnValue
+};
+
+function reduceToAnyTrue(sourceArray){
+    let returnValue = false
+    sourceArray.forEach(function(value){
+      if (value == true){
+        returnValue = true
+      }
+    })
+    return returnValue
+  }
