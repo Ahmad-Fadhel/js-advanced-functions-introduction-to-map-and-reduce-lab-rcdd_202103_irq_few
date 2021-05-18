5a66a4509d1247a46d940fda3760a98cdfa7db1b
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-05-18 22:54:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
 // Your code here
-const mapToNegativize = mapToNegativize.map(negative => {
-  return -negative
-})
+function mapToNegativize(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++ ) {
+      newArray.push(-1 * array[i])
+    }
+    return newArray
+  }
 
-const mapToNoChange =mapToNoChange.map(original => {
-  return original
-})
+  function mapToNoChange(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++ ) {
+      newArray.push(array[i])
+    }
+    return newArray
+  }
+
+  function mapToDouble(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++ ) {
+      newArray.push(2 * array[i])
+    }
+    return newArray
+  }
+
+  function mapToSquare(array) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++ ) {
+      newArray.push(array[i] ** 2)
+    }
+    return newArray
+  }
+
+  function reduceToTotal(array, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i < array.length; i++ ) {
+      total = total + array[i]
+    }
+    return total
+  }
+
+  function reduceToAllTrue(array) {
+    for (let i = 0; i < array.length; i++ ) {
+      if (!array[i]) return false
+    }
+    return true
+  }
+
+  function reduceToAnyTrue(array) {
+    for (let i = 0; i < array.length; i++ ) {
+      if (array[i]) return true
+    }
+    return false
+  } 
